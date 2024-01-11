a095947776e0e8ec6b05283b64ed21ed99d96e33
bbartika <137605070+bbartika@users.noreply.github.com>
2024-01-11 23:48:40 +0530

    Create Archived.js

---
+++ Models/Archived.js
@@ -0,0 +1,30 @@
+const{DataTypes}=require('sequelize')
+const sequelize=require('../utils/database')
+
+const AchivedChat = sequelize.define('achivedchat', {
+    id: {
+        type:DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    messageContent:{
+        type:DataTypes.TEXT,
+        allowNull: false
+    },
+    userName:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    userId:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    groupId:{
+        type:DataTypes.STRING,
+        allowNull:false
+    }
+
+})
+
+module.exports = AchivedChat
