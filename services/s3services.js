b6426f464fc7d0276ef05abd32a1ab27cfcf6732
bbartika <137605070+bbartika@users.noreply.github.com>
2024-01-12 00:02:12 +0530

    Create s3services.js

---
+++ services/s3services.js
@@ -0,0 +1,35 @@
+const AWS=require('aws-sdk')
+require('dotenv').config();
+
+const uploadToS3=(data,fileName)=>{
+    const BUCKET_NAME="expense-demo-tracker"
+   
+    let s3bucket=new AWS.S3({
+        accessKeyId:process.env.IAM_USER_KEY,
+        secretAccessKey:process.env.IAM_USER_SECRET,
+       
+    })
+    
+        let params={
+            Bucket:BUCKET_NAME,
+            Key:fileName,
+            Body:data.buffer,
+            ACL:'public-read'
+
+        }
+       return new Promise((resolve,reject)=>{
+        s3bucket.upload(params,(err,response)=>{
+            if(err){
+                reject('something went wrong',err)
+            }
+            else{
+                resolve(response.Location) 
+
+            }
+    }) 
+
+       })    
+}
+module.exports={
+    uploadToS3
+}
