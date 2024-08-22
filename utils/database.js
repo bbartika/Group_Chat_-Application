d6b9f8f3b4f109f998461039441c25ab6a499cae
bbartika <137605070+bbartika@users.noreply.github.com>
2024-08-22 13:06:29 +0530

    using MySQL

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,6 @@
 const Sequelize=require('sequelize').Sequelize
 require('dotenv').config();
 
-
 const sequelize=new Sequelize('chatapp','root','uitIT$1822',{
     dialect:'mysql',
     host:'localhost',
